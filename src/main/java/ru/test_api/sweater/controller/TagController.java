a35ef8ea98b4caf97cb06e1cc2fad813d5889eaf
suren <elek34ka@gmail.com>
2021-01-04 16:27:30 +0300

    tag model added

---
+++ src/main/java/ru/test_api/sweater/controller/TagController.java
@@ -0,0 +1,22 @@
+package ru.test_api.sweater.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.test_api.sweater.entity.Tag;
+import ru.test_api.sweater.repository.TagRepository;
+
+@RestController
+@RequestMapping("tag")
+public class TagController {
+    private TagRepository tagRepository;
+
+    public TagController(TagRepository tagRepository) {this.tagRepository = tagRepository;}
+
+    @GetMapping
+    public List<Tag> tagList() {return tagRepository.findAll();}
+    
+}
